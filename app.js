99ef37130cce4d63915210f1251ac8e59e76f05e
Vinicius Nunes Macedo <vnmacedo@gmail.com>
2019-06-14 19:29:42 -0300

    Conclusão do cadastro de Categoria

--- app.js
+++ app.js
@@ -5,13 +5,31 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
+const session = require("express-session");
+const flash = require("connect-flash");
 
 // Referencias das rotas da aplicação
 const admin = require('./routes/admin');
-const post = require('./routes/post');
-const categoria = require('./routes/categoria');
+//const post = require('./routes/post');
+//const categoria = require('./routes/categoria');
 
 // configurações
+
+    // Sessão
+    app.use(session({
+        secret: "nodedemo",
+        resave: true,
+        saveUninitialized: true
+    }));
+    app.use(flash());
+
+    // Midlleware
+    app.use((req, res, next) =>{
+        res.locals.success_msg = req.flash("success_msg");
+        res.locals.error_msg = req.flash("error_msg");
+        next();
+    });
+
     // Body Parser
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
@@ -21,16 +39,19 @@ const categoria = require('./routes/categoria');
     app.set('view engine', 'handlebars');
 
     // Mongoose
-    mongoose.connect("mongodb://localhost/mongoblog")
+    mongoose.connect("mongodb://localhost/mongoblog").then(() => {
+        console.log("Conectado ao servidor MongoDB.");
+    }).catch((err) => {
+        console.log("Erro ao se conectar ao banco: " + err);
+    });
 
     // Public 
     app.use(express.static(path.join(__dirname, "public")));
     
 // Rotas
 app.use('/admin', admin);
-app.use('/post', post);
-app.use('/categoria', categoria);
-
+//app.use('/post', post);
+//app.use('/categoria', categoria);
 
 // Outros
 const PORT = 8083
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require('mongoose');
+
+require("../models/Categoria");
+
+const Categoria = mongoose.model("categorias");
 
 router.get('/', (req, res) => {
     //res.render(__dirname +'/views/layouts/admin/index');
@@ -11,11 +16,95 @@ router.get('/posts', (req, res) => {
 });
 
 router.get('/categorias', (req, res) => {
-    res.render('layouts/admin/categorias');
+    Categoria.find().sort({date: 'desc'}).then((categorias) => {
+        res.render('layouts/admin/categorias',{categorias: categorias});
+    }).catch((err) =>{
+        req.flash("error_msg", "Erro ao listar: " + err);
+        res.redirect("/admin");
+    });
+   
 });
 
 router.get('/categorias/adicionar', (req, res) => {
-    res.render('layouts/admin/categoriasAdicionar');
+    res.render('layouts/admin/categoriaAdicionar');
+});
+
+router.get('/categorias/editar/:id', (req, res) => {
+    Categoria.findOne({_id:req.params.id}).then((categoria) =>{
+        res.render('layouts/admin/categoriaEditar', {categoria: categoria});
+    }).catch((err) => {
+        req.flash("error_msg", "Esta categoria não existe");
+        res.redirect("/admin/categorias");
+    });
+});
+
+router.post('/categorias/editar', (req, res) => {
+    Categoria.findOne({_id:req.body.id}).then((categoria) => {
+        
+        categoria.nome = req.body.nome;
+        categoria.slug = req.body.slug;
+
+        categoria.save().then(() => {
+
+            req.flash("success_msg", "Categoria criada com sucesso");
+            res.redirect("/admin/categorias");
+
+        }).catch((err) => {
+            
+            req.flash("error_msg", "Erro ao Editar: " + err);
+            res.redirect("/admin/categorias");
+        });
+    }).catch((err) => {
+        req.flash("error_msg", "Erro ao Editar: " + err);
+        res.redirect("/admin/categorias");
+    });
+});
+
+router.post('/categorias/excluir',(req, res) => {
+    Categoria.remove({_id: req.body.id}).then(() => {
+
+        req.flash("success_msg", "Categoria excluida com sucesso");
+        res.redirect("/admin/categorias");
+
+    }).catch((err) => {
+        
+        req.flash("error_msg", "Erro ao Excluir: " + err);
+        res.redirect("/admin/categorias");
+    });;
+})
+
+router.post('/categorias/novo', (req, res) => {
+    
+    const erros = [];
+    
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: "Nome inválido"});
+    }
+    
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+        erros.push({texto: "Slug inválido"});
+    }
+
+    if(req.body.nome.length < 2){
+        erros.push({texto: "Nome da Categoria muito pequeno"});
+    }
+
+    if(erros.length > 0){
+        res.render("layouts/admin/categoriasAdicionar", {erros: erros});
+    }else{
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        };
+
+        new Categoria(novaCategoria).save().then(()=>{
+            req.flash("success_msg", "Categoria criada com sucesso");
+            res.redirect("/admin/categorias");
+        }).catch((err) => {
+            req.flash("error_msg", "Erro ao salvar: " + err);
+            res.redirect("/admin");
+        });
+    }
 });
 
 module.exports = router
