beb4c13fd477f1a7117c326271baa51af2eea0e1
Vinicius Nunes Macedo <vnmacedo@gmail.com>
2019-06-25 17:24:12 -0300

    Alteração da string de conexão

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: "mongodb+srv://mongoteste:mongoteste@cluster0-qf7xf.mongodb.net/test?retryWrites=true&w=majority"}
+    module.exports = {mongoURI: "mongodb+srv://mongoteste:mongoteste@cluster0-qf7xf.mongodb.net/mongoblog?retryWrites=true&w=majority"}
 }else{
     module.exports = {mongoURI: "mongodb://localhost/mongoblog"}
 }
